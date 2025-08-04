3a599b9e8905361382b1d40690a0996b9c02f575
Elvis <elvisboves@Elviss-MBP.attlocal.net>
2025-08-04 19:28:37 -0400

    Start documenting ideas

---
+++ main.py
@@ -0,0 +1,14 @@
+# TODO: import modules yfinance
+# TODO: get data from stock
+# TODO: Make it searchable
+# TODO: print important data
+# TODO: Learn more about UX using Tkinter
+# TODO: Save info in a SQL data base
+
+
+import Tkinter as tk
+
+
+
+
+print("Hello World!")
